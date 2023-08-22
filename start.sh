689c2041e1eec6938858ef3d5228e6db3b83f4ba
Gitkup <obputer@gmail.com>
2023-08-22 10:09:11 -0400

    Updates

--- start.sh
+++ start.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+# ensure all dependencies are resolved
 sh "$(dirname "$0")/check.sh"
 if [ $? -eq 0 ]; then
 
+# change the working directory to conteni's
 cd "$(dirname "$0")"
+
+# print version
 ruby "version.rb"
 echo "Ctrl + C to quit"
 
+# compare changes since last update
+ruby "compare.rb"
+
+# archive and update
 sleep $(jq -r '.period' "config.json")
+ruby "archive.rb"
 git pull
 clear
 sh start.sh
